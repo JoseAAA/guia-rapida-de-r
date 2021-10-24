d189a951456314aef8e39880258d9d069de8e599
Jose Armando Alarcon Arone <jalarcon@camposol.com.pe>
2021-10-24 16:37:17 -0500

    Tercera clase

--- Codigo/01 R base/08 Familia apply.R
+++ Codigo/01 R base/08 Familia apply.R
@@ -6,10 +6,53 @@
 #                                                                         #
 ###########################################################################
 
+matriz <- matrix(1:12, nrow = 4)
+
+for (i in 1:4) {
+  print(sum(matriz[i,]))
+}
+
+for (j in 1:3) {
+  print(sum(matriz[,j]))
+}
 
 # Apply -------------------------------------------------------------------
+# MARGIN = 1 filas
+# MARGIN = 2 columnas
 
+suma_mayor_a <- function(x,a) {
+  
+  result <- ""
+  
+  if(sum(x) < a){
+    result <- paste("menor que",a)
+  } else{
+    result <- paste("mayor igual que",a)
+  }
+  
+  return(result)
+}
+
+apply(matriz, MARGIN = 1, FUN = sum)
+apply(matriz, MARGIN = 1, FUN = suma_mayor_a, a = 15)
+apply(matriz, MARGIN = 2, FUN = sum)
+apply(matriz, MARGIN = 2, FUN = suma_mayor_a, a = 15)
+
+# Del data.frame "iris", sacar la media y desviación estandar 
+# de las columnas cuantitativas con la función apply
+
+apply(iris[,1:4],MARGIN = 2,FUN = mean)
+apply(iris[,1:4],MARGIN = 2,FUN = sd)
+apply(iris[,1:4],MARGIN = 2,FUN = quantile, probs = c(0.2,0.8))
+
+apply(iris[iris$Species == "setosa",1:4],MARGIN = 2,FUN = mean)
+apply(iris[iris$Species == "versicolor",1:4],MARGIN = 2,FUN = mean)
+apply(iris[iris$Species == "virginica",1:4],MARGIN = 2,FUN = mean)
 
 # Lapply ------------------------------------------------------------------
+library(dplyr)
 
+iris_lista <- iris %>% 
+  split(.$Species)
 
+lapply(iris_lista, summary)
--- Codigo/01 R base/09 Importación y Exportación de Datos.R
+++ Codigo/01 R base/09 Importación y Exportación de Datos.R
@@ -6,25 +6,35 @@
 #                                                                         #
 ###########################################################################
 
-library(readr) 
-library(readxl)
-library(openxlsx)
-library(haven)
-library(DBI)
-library(odbc)
+library(readr) # csv
+library(readxl) # Importar excel
+library(openxlsx) # importar y exportar excel
+library(haven) # sav (spss)
+library(DBI) # Conexión a base de datos
+library(odbc) # Conexión a base de datos
 
 # csv ---------------------------------------------------------------------
+df <- read.csv("Datos/datos.csv")
+class(df)
+df <- read_csv("Datos/datos.csv")
+class(df)
+
+df_resultado <- df[df$edad > mean(df$edad),2:4]
+
+write.csv(df_resultado,"Datos/resultado.csv")
 
 # Excel -------------------------------------------------------------------
+df <- read_excel("Datos/datos.xlsx",sheet = 1)
+df <- read_excel("Datos/datos.xlsx",sheet = 4,range = "C7:E10")
+
+df_resultado <- df[df$edad > mean(df$edad),]
+
+write.xlsx(df_resultado,"Datos/resultado.xlsx")
 
 # sql ---------------------------------------------------------------------
-con <- dbConnect(odbc(), 
-                 Driver = "SQL Server", 
-                 Server = "Csplcdb02\\Prdfractal", 
-                 Database = "CMPPRONOSTICOS", 
-                 UID      = "proyeccion",
-                 PWD      = "Pr0yecci0n19")
-
-# Rdata -------------------------------------------------------------------
 
 
+# RData -------------------------------------------------------------------
+save(con,file = "Datos/conexion.RData")
+
+load("Datos/conexion.RData")
